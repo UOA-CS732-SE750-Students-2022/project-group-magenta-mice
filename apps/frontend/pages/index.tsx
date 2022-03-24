da895b41791b29b9c3c8f5cecc42c1be8bf58cad
Bruce Zeng <bruce.zeng@xtra.co.nz>
2022-03-24 15:50:19 +1300

    added inputs

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -106,6 +106,58 @@ export function Index() {
         <div>
           <DividedText text="Or sign in with email" />
         </div>
+
+        <div>
+          <form className="bg-transparent rounded px-8 pb-8 mb-4">
+            <div className="mb-4">
+              <label
+                className="block text-white text-sm font-bold"
+                htmlFor="username"
+              >
+                Username
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Your Email"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                className="block text-white text-sm font-bold"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="Your password"
+              />
+              <p className="text-red-500 text-xs italic">
+                Please choose a password.
+              </p>
+            </div>
+            <div className="flex items-center justify-between">
+              <a
+                className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                href="#"
+              >
+                Forgot Password?
+              </a>
+            </div>
+            <div>
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Sign In
+              </button>
+            </div>
+          </form>
+        </div>
       </Glass>
     </div>
   );
