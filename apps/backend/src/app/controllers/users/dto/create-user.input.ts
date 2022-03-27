272cfa892b50f1cec4207fe7ac4e97d429c5bca5
Bruce Zeng <bruce.zeng@xtra.co.nz>
2022-03-27 19:05:33 +1300

    Remove description of name

--- apps/backend/src/app/controllers/users/dto/create-user.input.ts
+++ apps/backend/src/app/controllers/users/dto/create-user.input.ts
@@ -5,9 +5,9 @@ export class CreateUserInput {
   @Field(() => ID, { description: "Google Id" })
   id: string;
 
-  @Field({ description: "Name of user" })
+  @Field()
   name: string;
 
-  @Field({nullable : true, description: "Google ProfilePic of user" })
+  @Field({ nullable: true, description: "Google ProfilePic of user" })
   profilePicUrl: string;
 }
