a2d5741372874acbfbe1d4cf22ac83202c0465b2
Bruce <64149662+BruceZeng1@users.noreply.github.com>
2022-03-27 19:02:52 +1300

    Update libs/components/src/lib/login-panel/index.tsx
    Co-authored-by: Fraser McCallum  <55963097+frasermcc9@users.noreply.github.com>

--- libs/components/src/lib/login-panel/index.tsx
+++ libs/components/src/lib/login-panel/index.tsx
@@ -114,10 +114,6 @@ export const useLoginPanelController = () => {
   const onClick = () => {
     signInWithPopup(auth, provider)
       .then(async (result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential?.accessToken;
-        // The signed-in user info.
         const user = result.user;
         createUser({
           variables: {
