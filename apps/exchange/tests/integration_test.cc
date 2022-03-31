807ea9f59908ac3dc2493eefd411b015153f3afd
FRASER MCCALLUM <fraser@themccallums.net.nz>
2022-03-31 21:07:54 +1300

    Add cancelling integration test

--- apps/exchange/tests/integration_test.cc
+++ apps/exchange/tests/integration_test.cc
@@ -1,5 +1,7 @@
 #include "test_common.h"
 
+#include <tuple>
+
 namespace Sim::Testing
 {
     class IntegrationTestFixture : public Test
@@ -44,13 +46,24 @@ namespace Sim::Testing
             return request;
         }
 
+        std::pair<Protocol::InsertOrderRequest, Protocol::CancelOrderRequest> makeOrderWithCancel(
+            uint32_t client,
+            RequestWrapper requestOptions)
+        {
+            Protocol::InsertOrderRequest insertRequest = makeOrder(client, requestOptions);
+            Protocol::CancelOrderRequest cancelRequest;
+            cancelRequest.set_clientid(insertRequest.clientid());
+
+            return std::make_pair(insertRequest, cancelRequest);
+        }
+
         Exchange mExchange;
 
         std::unordered_map<uint32_t, uint32_t> mParticipantOrders;
 
         std::shared_ptr<Participant> mParticipant1;
         std::shared_ptr<Participant> mParticipant2;
-    };
+    }; // namespace Sim::Testing
 
     TEST_F(IntegrationTestFixture, TestOrderFlow)
     {
@@ -103,5 +116,39 @@ namespace Sim::Testing
 
         EXPECT_EQ(mParticipant2->getCash(), -1 * (100 * 5 + 99 * 8));
         EXPECT_EQ(mParticipant2->getPosition(0), 13);
+    }
+
+    TEST_F(IntegrationTestFixture, TestOrderFlowWithCancels)
+    {
+        auto [ask1, cancelAsk1] = makeOrderWithCancel(
+            0,
+            {
+                .mInstrumentId = 0,
+                .mLifespan = Protocol::InsertOrderRequest::GFD,
+                .mSide = Protocol::InsertOrderRequest::SELL,
+                .mPrice = 100,
+                .mVolume = 5,
+
+            });
+
+        auto bid1 = makeOrder(
+            1,
+            {
+                .mInstrumentId = 0,
+                .mLifespan = Protocol::InsertOrderRequest::FAK,
+                .mSide = Protocol::InsertOrderRequest::BUY,
+                .mPrice = 100,
+                .mVolume = 3,
+
+            });
+
+        mParticipant1->requestOrderInsert(ask1);
+        ASSERT_EQ(mExchange.getOrderbook(0).getTopAsk()->get()->mVolume, 5);
+
+        mParticipant2->requestOrderInsert(bid1);
+        ASSERT_EQ(mExchange.getOrderbook(0).getTopAsk()->get()->mVolume, 2);
+
+        mParticipant1->requestOrderCancel(cancelAsk1);
+        ASSERT_EQ(mExchange.getOrderbook(0).getNumSellOrders(), 0);
     }
 } // namespace Sim::Testing
