25a051ba4c8bed9a2aae51ca465c97bb8a42615c
Fraser McCallum <55963097+frasermcc9@users.noreply.github.com>
2022-04-16 13:40:45 +1200

    Add layout component - Page and Footer impl (#15)

--- libs/components/src/index.ts
+++ libs/components/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./lib/divided-text"
 export * from "./lib/glass"
 export * from "./lib/haze"
+export * from "./lib/layout"
 export * from "./lib/loading"
 export * from "./lib/login-panel"
