edda90c90af17cb2b88509e485d89d3c75b9bccc
James <54062686+nzbasic@users.noreply.github.com>
2022-04-18 19:01:19 +1200

    add logic for redirecting unauth

--- apps/frontend/pages/invite/[hash].tsx
+++ apps/frontend/pages/invite/[hash].tsx
@@ -1,12 +1,24 @@
 import { Layout } from "@simulate-exchange/components";
 import { useRouter } from "next/router";
+import { useUser } from "@simulate-exchange/hooks";
+import { useEffect } from "react";
 
 export function Invite() {
   const router = useRouter()
+  const { user, loading } = useUser()
   const { hash } = router.query
 
+  useEffect(() => {
+    if (router.isReady && !user && !loading) {
+      router.push(`/auth?invite=${hash}`)
+    }
+
+    // todo: verify that invite is legit
+  }, [router, user, hash, loading])
+
+  if (!user) return <></>
   return (
-    <Layout.Page>{hash}</Layout.Page>
+    <Layout.Page>{hash} - {user.email}</Layout.Page>
   );
 }
 
