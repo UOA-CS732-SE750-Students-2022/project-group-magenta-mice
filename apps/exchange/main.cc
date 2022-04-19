2530206956ba49fa21897b470b6a8993b9b5f4ae
FRASER MCCALLUM <fraser@themccallums.net.nz>
2022-04-19 18:09:10 +1200

    Add example query

--- apps/exchange/main.cc
+++ apps/exchange/main.cc
@@ -1,8 +1,10 @@
 #include <boost/asio.hpp>
 #include <common/timer.h>
 #include <config/config_reader.h>
+#include <db/connection.h>
 #include <iostream>
 #include <net/exchange_server.h>
+#include <pqxx/pqxx>
 
 int main(int argc, char* argv[])
 {
@@ -17,6 +19,18 @@ int main(int argc, char* argv[])
     auto config = reader.read(argv[1]);
     auto port = config.getPort();
     auto instruments = config.getInstruments();
+    auto dbString = config.getDbString();
+
+    Sim::Db::Connection dbService{ dbString };
+
+    auto result = dbService.exec([](pqxx::work& W) {
+        return W.exec_params("SELECT * FROM public.\"User\" WHERE id=$1", "u5aJQIpSWWRsLktczu0XVWMjWZH3");
+    });
+
+    for (const auto& r : result)
+    {
+        std::cout << r[1].as<std::string>() << '\n';
+    }
 
     io::io_context ioContext;
     Sim::Net::ExchangeServer server(ioContext, port);
