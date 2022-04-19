8bbf0bf10c1f45c64d75d4692b29b147dec92570
Fraser McCallum <55963097+frasermcc9@users.noreply.github.com>
2022-04-19 22:14:11 +1200

    Add PR checks in CICD (#21)
    * Add PR checks in CICD
    
    * Do not run tests on release build

--- apps/exchange/release.sh
+++ apps/exchange/release.sh
@@ -2,4 +2,4 @@ protoc -I=. --cpp_out=. --python_out=./messaging/python ./libs/protocol/exchange
 mv ./messaging/python/libs/protocol/* ./messaging/python
 rm -rf ./messaging/python/libs
 cmake -DCMAKE_BUILD_TYPE=Release -B build -G 'Ninja'
-cmake --build build --config Release && spm run test
+cmake --build build --config Release
