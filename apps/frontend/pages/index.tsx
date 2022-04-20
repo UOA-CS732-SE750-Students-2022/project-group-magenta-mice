ba881805968044a5418e9e80e4363df9e46b83f8
James <54062686+nzbasic@users.noreply.github.com>
2022-04-20 18:04:31 +1200

    add exchange info to home page

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,9 +1,45 @@
-import { Layout } from "@simulate-exchange/components";
-import { useCurrentUserQuery } from "@simulate-exchange/gql";
+import { Layout, Loading } from "@simulate-exchange/components";
+import { useCreateTestExchangeMutation, useCurrentUserQuery } from "@simulate-exchange/gql";
+import { useFullLoader, useIsLoggedIn } from "@simulate-exchange/hooks";
+import { useRouter } from "next/router";
 
 export default function Index() {
-  const { data } = useCurrentUserQuery();
-  console.log(data);
+  const { loggedIn } = useIsLoggedIn();
+  useFullLoader(!loggedIn)
+  return <Layout.Page>{loggedIn ? <AuthComponent /> : <></>}</Layout.Page>
+}
 
-  return <Layout.Page></Layout.Page>;
+const AuthComponent = () => {
+  const { data, loading } = useCurrentUserQuery();
+  const [createTestExchange] = useCreateTestExchangeMutation()
+  const router = useRouter()
+
+  const onClick = async () => {
+    const { data } = await createTestExchange()
+    console.log(data)
+    router.push(`/exchange/${data.createTestExchange.id}`)
+  }
+
+  if (loading) return <Loading />
+  return (
+    <Layout.Page>
+      <div className="flex flex-col gap-2">
+        <span>Hi {data?.currentUser.name}</span>
+        <div>
+          <span>Your Exchanges:</span>
+          {data?.currentUser.userPermissions.map(permission => (
+            <div key={permission.exchange.id} className="flex gap-1">
+              <a className="text-blue-500" href={`/exchange/${permission.exchange.id}`}>{permission.exchange.id}</a>
+              <span>-</span>
+              <span>{permission.permission}</span>
+            </div>
+          ))}
+        </div>
+        <button
+          className="self-start bg-green-500 px-2 py-1 text-white rounded font-bold"
+          onClick={onClick}
+        >Create Test Exchange</button>
+      </div>
+    </Layout.Page>
+  )
 }
