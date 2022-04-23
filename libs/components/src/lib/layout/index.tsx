d7849ced24506424221cad2e3283a445d3a11b43
Fraser McCallum <fraser@themccallums.net.nz>
2022-04-23 23:59:16 +1200

    Add sidebar api + example sidebar on exchange page

--- libs/components/src/lib/layout/index.tsx
+++ libs/components/src/lib/layout/index.tsx
@@ -2,12 +2,13 @@ import { useRouter } from "next/router";
 import React from "react";
 import AppLogo from "../app-logo";
 import UserDropdown from "../user-dropdown";
+import cx from "classnames";
 
 const Header: React.FC = () => {
   const { push } = useRouter();
 
   return (
-    <div className="mb-8 bg-neutral-800 p-4 shadow">
+    <div className="z-10 bg-neutral-800 p-4 shadow-lg">
       <div className="mx-6 flex items-center justify-between sm:mx-10">
         <button
           className="h-auto w-auto bg-transparent sm:ml-0 "
@@ -33,15 +34,34 @@ const Footer: React.FC = () => {
   );
 };
 
-const Page: React.FC = ({ children }) => {
-  return (
-    <div className="flex min-h-screen w-full flex-col bg-neutral-900">
-      <Header />
-      <div className="w-full max-w-6xl flex-grow self-center px-10">
-        {children}
-      </div>
+interface PageProps {
+  sidebar?: React.ReactNode;
+  sidebarClassName?: string;
+}
 
-      <Footer />
+const Page: React.FC<PageProps> = ({ children, sidebar, sidebarClassName }) => {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <div className="flex h-full flex-grow">
+        {sidebar && (
+          <div
+            className={cx(
+              "hidden max-w-lg bg-neutral-800 xl:block",
+              sidebarClassName,
+            )}
+          >
+            {sidebar}
+          </div>
+        )}
+        <div className="flex w-full flex-col bg-neutral-900">
+          <div className="w-full max-w-6xl flex-grow self-center px-10 pt-8">
+            {children}
+          </div>
+
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 };
