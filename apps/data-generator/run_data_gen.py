93a454842aa7756d924fc023f9d26b435888cf8b
Fuki-UoA <fbab920@aucklanduni.ac.nz>
2022-05-01 16:49:59 +1200

    Adjust to interface changes

---
+++ apps/data-generator/run_data_gen.py
@@ -0,0 +1,23 @@
+from src.DataServer import DataServer
+
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Runs data server')
+    parser.add_argument('-log', 
+                        help='Logs order objects on command line without starting socket.',
+                        action='store_true'
+                        )
+    args = parser.parse_args()
+
+    ds = DataServer(
+        instrument_id=0,
+        init_price=100, 
+        volatility=10, 
+        max_position_limit=10,
+    )
+    
+    if args.log:
+        ds.log_orders()
+    else:
+        ds.run()
