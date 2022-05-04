e9d0482b1765a2aaa3485065fe814c8be6de7b58
BJ <kelmeld@outlook.com>
2022-05-04 14:51:55 +1200

    mock the router and the image

--- apps/frontend/specs/index.spec.tsx
+++ apps/frontend/specs/index.spec.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import mockRouter from "next-router-mock";
 
 import Index from "../pages/index";
 
+jest.mock("next/router", () => require("next-router-mock"));
+jest.mock("next/image", () => ({
+  __esModule: true,
+  default: () => {
+    return "Next image stub"; // whatever
+  },
+}));
+
 describe("Index", () => {
+  beforeEach(() => {
+    mockRouter.setCurrentUrl("/");
+  });
+
   it("should render successfully", () => {
     const { baseElement } = render(<Index />);
     expect(baseElement).toBeTruthy();
