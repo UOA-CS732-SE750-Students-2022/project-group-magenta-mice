65a58351e37f77bc36414cd5dfb57fcd1d263ab6
BJ <kelmeld@outlook.com>
2022-05-09 19:14:59 +1200

    add testing the dropdown on /exchange

--- apps/frontend-e2e/src/integration/exchange.spec.ts
+++ apps/frontend-e2e/src/integration/exchange.spec.ts
@@ -18,21 +18,48 @@ describe("Exchange Page authenticated", () => {
   });
   it("authenticated user should stay in /exchange", () => {
     cy.url().should("include", "/exchange");
+    cy.contains("welcome", { matchCase: false }).should("exist");
   });
 
-  it("clicking add exchange card opens create exchange modal", () => {
-    cy.get("#addCard").click();
-    cy.get("h3")
-      .contains("create an exchange", { matchCase: false })
-      .should("be.visible");
-    cy.get("button").contains("cancel", { matchCase: false }).click();
+  describe("Create Exchange Modal", () => {
+    it("clicking add exchange card opens create exchange modal", () => {
+      cy.get("#addCard").click();
+      cy.get("h3")
+        .contains("create an exchange", { matchCase: false })
+        .should("be.visible");
+      cy.get("button").contains("cancel", { matchCase: false }).click();
+    });
+
+    it("clicking cancel button on create exchange modal should close it", () => {
+      cy.get("#addCard").click();
+      cy.get("button").contains("cancel", { matchCase: false }).click();
+      cy.get("h3")
+        .contains("create an exchange", { matchCase: false })
+        .should("not.exist");
+    });
   });
 
-  it("clicking cancel button on create exchange modal should close it", () => {
-    cy.get("#addCard").click();
-    cy.get("button").contains("cancel", { matchCase: false }).click();
-    cy.get("h3")
-      .contains("create an exchange", { matchCase: false })
-      .should("not.exist");
+  describe("User Dropdown", () => {
+    it("clicking on user should make dropdown appear", () => {
+      cy.get("#headlessui-popover-button-1").click();
+      cy.get("button").contains("logout", { matchCase: false }).should("exist");
+      cy.get("#headlessui-popover-button-1").click();
+    });
+
+    it("clicking on user twice should make dropdown disappear", () => {
+      cy.get("#headlessui-popover-button-1").click();
+      cy.get("#headlessui-popover-button-1").click();
+      cy.get("button")
+        .contains("logout", { matchCase: false })
+        .should("not.exist");
+    });
+
+    it("clicking on logout button logs user out", () => {
+      cy.get("#headlessui-popover-button-1").click();
+      cy.get("button").contains("logout", { matchCase: false }).click();
+      cy.url().should("include", "/landing");
+      cy.visit("/exchange");
+      cy.url().should("include", "/landing");
+    });
   });
 });
