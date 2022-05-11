1c1afe227cc146b60932831efaf3255a34da0190
Fuki <60748192+Fuki-UoA@users.noreply.github.com>
2022-05-11 23:18:39 +1200

    Client WebSocket (#46)
    * Remove running state
    
    * Change to websocket
    
    * Change to secure connection
    
    * Refactor imports, code
    
    * Update package requirements
    
    * Import bug fixes

--- apps/data-generator/libs/ExchangeClient.py
+++ apps/data-generator/libs/ExchangeClient.py
@@ -1,73 +1,55 @@
 from enum import Enum
 from InsertOrder import InsertOrder, LifeSpan, Side
-from LoginResponse import LoginResponse
-import socket, errno, sys, select
+import sys
 import exchange_pb2 as proto
 import State
 from time import sleep
 from typing import Callable
 from threading import Thread
 
-HEADER_LENGTH = 8
+from websocket import WebSocketApp
 
 class ExchangeClient:
     '''
     A client class responsible for sending InsertOrder object through socket.
     '''
     def __init__(self, hostname: str = '127.0.0.1', port: int = 15001):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((hostname, port))
-        self.socket.setblocking(True)
-
+        
+        self.uri = f'ws://{hostname}:{port}'
+        self.ws = WebSocketApp(self.uri, on_message=self.on_message, on_error=self.on_error)
+        
         # hash event_enum: Event => list[handler: Callable]
         self.handlers = {event_enum: [] for event_enum in event_to_class}
-        self.client_thread = Thread(target=self._run_client)
-
+ 
+        Thread(target=self.ws.run_forever).start()
+        # Waiting for websocket to connect.
+        sleep(1)
+        
         self.state = State.NotLoggedInState(self)
 
     def run(self):
         self.state.run()
 
-    def _run_client(self) -> None:
-        while True:
-            try:
-                (readable, _, _) = select.select(
-                    [self.socket], [], []
-                )
-
-                for s in readable:
-                    if s == self.socket:
-
-                        event_type_raw = s.recv(4)
-                        event_type = int.from_bytes(event_type_raw, byteorder="little")
-                        print(f"Message type: {event_type}")
-
-                        if not len(event_type_raw):
-                          print("Server closed connection")
-                          sys.exit()
-
-                        size_raw = s.recv(4)
-                        data_length = int.from_bytes(size_raw, byteorder="little")
-                        print(f"Message size: {data_length}")
-
-                        data = s.recv(data_length)
-
-                        event_enum = Event(event_type)
-                        event_proto_obj = event_to_class[event_enum]()
-                        event_proto_obj.ParseFromString(data)
-
-                        self.emit(event_enum, event_proto_obj)
-
-            except IOError as e:
-                sleep(1)
-                if e.errno == errno.EAGAIN or e.errno != errno.EWOULDBLOCK:
-                    continue
-
-                self.client_thread.join()
-                sys.exit(1)
-            except Exception:
-                self.client_thread.join()
-                sys.exit(1)
+    def on_message(self, ws, message) -> None:
+        # first 4 bytes is event type
+        event_type_raw = message[:4]
+        event_type = int.from_bytes(event_type_raw, byteorder="little")
+        
+        print(f"Message type: {event_type}")
+        
+        if not len(event_type_raw):
+            print("Server closed connection")
+            ws.close()
+            sys.exit()
+        
+        event_enum = Event(event_type)
+        event_proto_obj = event_to_class[event_enum]()
+        event_proto_obj.ParseFromString(message[4:])
+        
+        self.emit(event_enum, event_proto_obj)
+    
+    def on_error(self, ws, error):
+        print(error)
 
 
     def send_insert_request(self, insert_order: InsertOrder) -> None:
@@ -93,8 +75,8 @@ class ExchangeClient:
 
         return True
 
-    def send_login_request(self, key: str) -> LoginResponse:
-        return self.state.send_login_request(key)
+    def send_login_request(self, key: str) -> None:
+        self.state.send_login_request(key)
 
     def change_state(self, state: State) -> None:
         self.state = state
@@ -109,16 +91,17 @@ class Event(Enum):
 event_to_class = {
     Event.FEED: proto.ExchangeFeed,
     Event.FILL: proto.OrderFillMessage,
-    Event.UPDATE: proto.OrderUpdateMessage
+    Event.UPDATE: proto.OrderUpdateMessage,
+    Event.LOGINRESPONSE: proto.LoginResponse
 }
 
 if __name__ == '__main__':
     client = ExchangeClient()
     client.add_handler(Event.UPDATE, print)
-    res = client.send_login_request("6f0cb665-c2ea-4460-8442-ebfbe01fbedf")
+    client.add_handler(Event.LOGINRESPONSE, print)
+    
+    client.send_login_request("6f0cb665-c2ea-4460-8442-ebfbe01fbedf")
     # res = client.send_login_request("95e95dae-b722-4b2e-9c11-db7088041b63")
-    print(res)
-    client.run()
 
     sleep(4)
     client.send_insert_request(InsertOrder(5, 100, LifeSpan.GFD, Side.BUY, 0, 0))
--- apps/data-generator/test_server.py
+++ apps/data-generator/test_server.py
@@ -1,7 +1,9 @@
-import socket
 import struct
 import libs.exchange_pb2 as proto
 from time import sleep
+
+
+import websockets, asyncio
 
 EVENTS = {
     proto.EXCHANGE_FEED: proto.ExchangeFeed,
@@ -9,22 +11,14 @@ EVENTS = {
     proto.ORDER_UPDATE: proto.OrderUpdateMessage
 }
 
-if __name__ == '__main__':
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ip = '127.0.0.1'
-    port=15001
-    s.bind((ip, port))
-    s.listen(1)
-    c, addr = s.accept()
-    print ('Got connection from', addr )
+async def handler(websocket):
+    message = await websocket.recv()
+    #login message
+    #event_type = int.from_bytes(message[:4], byteorder='little')
+    content = message[4:]
     
-    # login message
-    event_type = int.from_bytes(c.recv(4), byteorder='little')
-    data_length = int.from_bytes(c.recv(4), byteorder='little')
-                    
-    data = c.recv(data_length)
     login_req = proto.LoginRequest()
-    login_req.ParseFromString(data)
+    login_req.ParseFromString(content)
     print(login_req)
     
     # login response
@@ -37,23 +31,82 @@ if __name__ == '__main__':
     instrument2.positionLimit = 10
     
     ser_login_res = login_res.SerializeToString()
-    c.send(
-            struct.pack('<i', proto.LOGIN) 
-            + struct.pack('<i', len(ser_login_res))
+    await websocket.send(
+            struct.pack('<i', proto.LOGIN_RESPONSE)
             + ser_login_res
             )
-    
-    
+
     while True:
         obj = proto.OrderUpdateMessage()
         obj.volumeRemaining = 100
         obj_ser = obj.SerializeToString()
-
-        c.send(
-            struct.pack('<q', 100) 
-            + struct.pack('<i', proto.ORDER_UPDATE) 
-            + struct.pack('<i', len(obj_ser))
-            + obj_ser 
+        
+        await websocket.send(
+            struct.pack('<i', proto.ORDER_UPDATE)
+            + obj_ser
         )
+        
         sleep(1)
+    
+    # # for insert order
+    # message = await websocket.recv()
+    # print(message)
+        
+
+async def main():
+    ip = '127.0.0.1'
+    port=15001
+    
+    async with websockets.serve(handler, ip, port):
+        await asyncio.Future()
+        
+if __name__ == '__main__':
+    asyncio.run(main())
+    
+    
+# if __name__ == '__main__':
+#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     ip = '127.0.0.1'
+#     port=15001
+#     s.bind((ip, port))
+#     s.listen(1)
+#     c, addr = s.accept()
+#     print ('Got connection from', addr )
+    
+#     # login message
+#     event_type = int.from_bytes(c.recv(4), byteorder='little')
+#     data_length = int.from_bytes(c.recv(4), byteorder='little')
+                    
+#     data = c.recv(data_length)
+#     login_req = proto.LoginRequest()
+#     login_req.ParseFromString(data)
+#     print(login_req)
+    
+#     # login response
+#     login_res = proto.LoginResponse()
+#     instrument = login_res.instruments.add()
+#     instrument.id = 1
+#     instrument.positionLimit = 10
+#     instrument2 = login_res.instruments.add()
+#     instrument2.id = 2
+#     instrument2.positionLimit = 10
+    
+#     ser_login_res = login_res.SerializeToString()
+#     c.send(
+#             struct.pack('<i', proto.LOGIN) 
+#             + struct.pack('<i', len(ser_login_res))
+#             + ser_login_res
+#             )
+    
+    
+#     while True:
+#         obj = proto.OrderUpdateMessage()
+#         obj.volumeRemaining = 100
+#         obj_ser = obj.SerializeToString()
+
+#         c.send(struct.pack('<i', proto.ORDER_UPDATE) 
+#             + struct.pack('<i', len(obj_ser))
+#             + obj_ser 
+#         )
+#         sleep(3)
         
