0a1a9d02bc93cd9a8f53183cb6c0ac033bfe7f46
James <54062686+nzbasic@users.noreply.github.com>
2022-05-15 15:47:40 +1200

    debugging

--- apps/data-generator/run_data_gen.py
+++ apps/data-generator/run_data_gen.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import time
 from src.DataServer import DataServer
 from src.generators.DataGenerator import DataGenerator
 
@@ -8,22 +8,24 @@ import argparse, json
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Runs data server')
-    parser.add_argument('-config_path', metavar='path/to/config', type=str, 
+    parser.add_argument('-config_path', metavar='path/to/config', type=str,
                         default='config.json',
                         help='Path to json config file')
-    parser.add_argument('-log', 
+    parser.add_argument('-log',
                         help='Logs order objects on command line without starting socket.',
                         action='store_true'
                         )
-    
+
     args = parser.parse_args()
-    
+
     with open(args.config_path, 'r') as config_file:
         config = json.load(config_file)
-        
+
     data_generators = []
     for instrument in config['instruments']:
         data_generators.append(DataGenerator(instrument))
+
+    time.sleep(5)
 
     ds = DataServer(
         data_generators=data_generators,
@@ -31,8 +33,8 @@ if __name__ == '__main__':
         hostname=config['host'],
         port=int(config['port'])
     )
-    
-        # if args.log:
-        #     ds.log_orders()
-        # else:
-        #     ds.run()
+
+    # if args.log:
+    #     ds.log_orders()
+    # else:
+    #     ds.run()
