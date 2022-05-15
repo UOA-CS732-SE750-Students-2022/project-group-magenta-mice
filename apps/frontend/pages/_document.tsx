40d5494fda2f00038ba39327056aae3bd8b0aa4f
James <54062686+nzbasic@users.noreply.github.com>
2022-05-15 22:02:48 +1200

    font link

--- apps/frontend/pages/_document.tsx
+++ apps/frontend/pages/_document.tsx
@@ -25,6 +25,11 @@ export default function Document() {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <script
         async
